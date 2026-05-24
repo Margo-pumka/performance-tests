abfec9b733505a9788761b9b44bb42e90273cc43
Margarita Fedorova <fedorova.margarita.a@gmail.com>
2026-05-24 17:19:50 +0300

    Практикуемся в реализации gRPC API клиентов. Практика реализации gRPC API клиентов: OperationsGatewayService.

--- tools/faker.py
+++ tools/faker.py
@@ -1,5 +1,5 @@
 import time
-
+from google.protobuf.internal.enum_type_wrapper import EnumTypeWrapper
 from faker import Faker
 from faker.providers.python import TEnum
 
@@ -22,6 +22,15 @@ class Fake:
         :return: Случайное значение из перечисления.
         """
         return self.faker.enum(value)
+
+    def proto_enum(self, value: EnumTypeWrapper) -> int:
+        """
+        Выбирает случайное значение из proto enum-типа.
+
+        :param value: Proto enum-класс для генерации значения.
+        :return: Случайное значение из перечисления.
+        """
+        return self.faker.random_element(value.values())
 
     def email(self) -> str:
         """
