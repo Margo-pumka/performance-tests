9e294883dea2bf1df3ed7a5d7e8ccd734ec03f80
Margarita Fedorova <fedorova.margarita.a@gmail.com>
2026-06-15 23:48:22 +0300

    Знакомство с Locust.Практика: написание нагрузочного сценария открытия дебетового счёта.

--- locust_open_debit_card_account.py
+++ locust_open_debit_card_account.py
@@ -1,27 +1,24 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, between, task
 
 from tools.faker import fake
 
 
-class GetUserScenarioUser(HttpUser):
+class OpenDebitCardAccountScenarioUser(HttpUser):
     wait_time = between(1, 3)
     user_data: dict
 
     def on_start(self) -> None:
         request = {
             'email': fake.email(),
-            'first_name':  fake.first_name(),
+            'first_name': fake.first_name(),
             'last_name': fake.last_name(),
             'middle_name': fake.middle_name(),
             'phone_number': fake.phone_number(),
         }
         response = self.client.post('/api/v1/users', json=request)
+
         self.user_data = response.json()
 
-
     @task
-    def get_user(self):
-        self.client.get(
-            f'/api/v1/users/{self.user_data['user']['id']}',
-            name='/api/v1/users/{user_id}'
-        )
+    def open_debit_card_account(self):
+        self.client.post('/api/v1/accounts/open-debit-card-account', json={'userId': self.user_data['user']['id']})
