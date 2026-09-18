d9963e0724b9e9c491a02f07c466e367c2f8deed
Margarita Fedorova <fedorova.margarita.a@gmail.com>
2026-09-18 22:56:55 +0300

    Revert

--- clients/http/gateway/client.py
+++ clients/http/gateway/client.py
@@ -6,7 +6,7 @@ from clients.http.event_hooks.locust_event_hook import locust_request_event_hook
 
 
 def build_gateway_http_client() -> Client:
-    
+    return Client(timeout=100, base_url="http://localhost:8003")
 
 
 def build_gateway_locust_http_client(environment: Environment) -> Client:
