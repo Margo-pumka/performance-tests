51fce93c675e68d32b44ce08c3fe6376ab02b999
Margarita Fedorova <fedorova.margarita.a@gmail.com>
2026-09-18 14:12:52 +0300

    Практика нагрузочного тестирования. Часть 2. Практика: Получение операций существующим пользователем.

--- clients/http/gateway/operations/schema.py
+++ clients/http/gateway/operations/schema.py
@@ -18,6 +18,7 @@ class OperationStatus(StrEnum):
     FAILED = 'FAILED'
     COMPLETED = 'COMPLETED'
     IN_PROGRESS = 'IN_PROGRESS'
+    UNSPECIFIED = 'UNSPECIFIED'
 
 
 class GetOperationsQuerySchema(BaseModel):
