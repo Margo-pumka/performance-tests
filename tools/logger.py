a4f43da29016d99286970166c31f5bd5187df166
Margarita Fedorova <fedorova.margarita.a@gmail.com>
2026-09-23 23:43:28 +0300

    Добавляем логирование для нагрузочных тестов.

---
+++ tools/logger.py
@@ -0,0 +1,17 @@
+import logging
+
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if not logger.hasHandlers():
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.INFO)
+
+        formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+        handler.setFormatter(formatter)
+
+        logger.addHandler(handler)
+
+    return logger
